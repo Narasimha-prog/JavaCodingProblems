e83dad5e93e7a53cb4b189b7b8e4035b0dfd6981
LN REDDY <lnreddyreddy405@gmail.com>
2025-07-03 23:03:14 +0530

    quick sorting...

--- DSA/src/sortingAlgorithm/QuickSorting.java
+++ DSA/src/sortingAlgorithm/QuickSorting.java
@@ -8,17 +8,44 @@ public class QuickSorting {
         int low=0,high=nums.length-1;
 
        quickSort(nums,low,high);
+
+        for(int i: nums){
+            System.out.print(i+" ");
+        }
+
     }
 
     private static void quickSort(int[] nums, int low, int high) {
         if(low<high){
          int pi= partition(nums,low,high);
 
+         quickSort(nums,pi+1,high);
+         quickSort(nums,low,pi-1);
         }
     }
 
     private static int partition(int[] nums, int low, int high) {
 
-        return low;
+        int pivot=nums[high];
+        int i=low-1;
+    for(int j=low;j<high;j++){
+
+        if(nums[j]<pivot){
+            i++;
+            int temp= nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+
     }
+        int temp= nums[i+1];
+        nums[i+1]=nums[high];
+        nums[high]=temp;
+
+        return i+1;
+    }
+
+
+
+
 }
